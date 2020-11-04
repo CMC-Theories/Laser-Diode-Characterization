34943f12580b7700a620bd47dd2f759c179b23dc
Taylor Simpkins <tsimpkin@uoregon.edu>
2020-11-04 12:13:29 -0800

    update pulse to 100us

--- KeysightB22901AcurrentSweep.py
+++ KeysightB22901AcurrentSweep.py
@@ -5,7 +5,7 @@ RM.list_resources()
 SMU = RM.open_resource('<name of keysight b2901a>')
 SMU.write(":curr:mode sweep; start 0; stop 2; step 0.05")
 #sets the current source to be in sweep mode from 0-2A in 0.05A steps
-SMU.write(":func:puls; :puls:del 5E-05; widt 5E-05")
+SMU.write(":func:puls; :puls:del 1E-04; widt 1E-04")
 #sets pulse width of source output and delay to be 50 us, which is pulse width lower limit
 SMU.write(':sens:func:off curr; :sens:rem on; :sens:wait off')
 #turns off current sensing (which is on by default), turns on remote sensing (required for 4 wire setup)
