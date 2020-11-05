937bfecec82ee0e15fbd712888b8d80991892151
Taylor Simpkins <tsimpkin@uoregon.edu>
2020-11-05 14:46:49 -0800

    added comments

--- KeysightCurrSweepV2.py
+++ KeysightCurrSweepV2.py
@@ -1,23 +1,22 @@
 import pyvisa
 RM = pyvisa.ResourceManager()
 RM.list_resources()
-SMU = RM.open_resource('USB0::0x0957::0x8B18::MY51143520::INSTR')
-SMU.write("*RST")
-SMU.write('sour:func:mode curr')
-SMU.write('sour:func:shap puls')
-SMU.write('sour:curr:mode sweep')
-SMU.write('sour:curr:star 0')
-SMU.write('sour:curr:stop 2')
-SMU.write('sour:curr:step 0.05')
-SMU.write('puls:del 1E-03')
-SMU.write('puls:widt 1E-04')
-SMU.write('sens:volt:aper 1E-04')
-SMU.write('sens:volt:auto on')
-SMU.write('sens:rem on')
-SMU.write('form:elem:sens curr, time, sour')
-SMU.write('trac:feed sens')
-SMU.write('outp:filt off')
-SMU.write('outp:off:auto on')
-SMU.write('outp on')
-SMU.write('init (@1)')
-
+SMU = RM.open_resource('USB0::0x0957::0x8B18::MY51143520::INSTR') #names instrument
+SMU.write("*RST")                                                 #resets to all default settings
+SMU.write('sour:func:mode curr')                                  #sets source output to current
+SMU.write('sour:func:shap puls')                                  #sets source function to pulse
+SMU.write('sour:curr:mode sweep')                                 #sets source output to sweep
+SMU.write('sour:curr:star 0')                                     #sets curr sweep start to 0 A
+SMU.write('sour:curr:stop 2')                                     #sets curr sweep max to 2 A
+SMU.write('sour:curr:step 0.05')                                  #sweeps by 0.05 A steps
+SMU.write('puls:del 1E-03')                                       #pulse delay to 1 ms
+SMU.write('puls:widt 1E-04')                                      #pulse width to 0.1 ms  
+SMU.write('sens:volt:aper 1E-04')                                 #sensor reading integration @ 0.1 ms
+SMU.write('sens:volt:auto on')                                    #sensor volt range to auto
+SMU.write('sens:rem on')                                          #enables remote sensing
+SMU.write('form:elem:sens curr, time, sour')                      #data format for output
+SMU.write('trac:feed sens')                                       #save data to trace buffer
+SMU.write('outp:filt off')                                        #disables lowpass filter of output
+SMU.write('outp:off:auto on')                                     #allows output to turn off after sweep
+SMU.write('outp on')                                              #turns on output
+SMU.write('init (@1)')                                            #starts pulse sweep
