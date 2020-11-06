6bfefd98c4ab235b2b60f5fd73a3578645aa83ae
Chase Craig <cmctheory@hotmail.com>
2020-11-06 13:12:38 -0800

    Modified CurSweep

--- KeysightCurrSweepV2.py
+++ KeysightCurrSweepV2.py
@@ -1,4 +1,4 @@
-import pyvisa
+"""import pyvisa
 RM = pyvisa.ResourceManager()
 RM.list_resources()
 SMU = RM.open_resource('USB0::0x0957::0x8B18::MY51143520::INSTR') #names instrument
@@ -21,3 +21,40 @@ SMU.write('outp:filt off')                                        #disables lowp
 SMU.write('outp:off:auto on')                                     #allows output to turn off after sweep
 SMU.write('outp on')                                              #turns on output
 SMU.write('init (@1)')                                            #starts pulse sweep
+"""
+import pyvisa
+import numpy as np
+import matplotlib.pyplot as plt
+RM = pyvisa.ResourceManager()
+RM.list_resources()
+SMU = RM.open_resource('USB0::0x0957::0x8B18::MY51143520::INSTR')
+SMU.write("*RST")
+SMU.write("syst:lfr 60")
+SMU.write('sour:func:mode curr')
+SMU.write('sour:func:shap puls')
+SMU.write('sour:puls:del 1E-04')
+SMU.write('sour:puls:widt 1E-04')
+SMU.write('sour:curr 0.0')
+SMU.write('sens:rem on')
+SMU.write('sens:volt:prot:pos 5')
+SMU.write('form:elem:sens volt, curr')
+values = [0 for i in range(50)]
+SMU.write('outp on')
+SMU.write('init (@1)')
+def setCurRead(val):
+    SMU.write('sour:curr ' + val)
+    SMU.write('init (@1)')
+    return SMU.query('fetc:arr?')
+ind = np.arange(50)
+iv = (ind*ind)*2 / 50*50
+for k,v in enumerate(iv):
+    values[k] = setCurRead(v)
+
+SMU.write('outp off')
+values = np.array(values)
+np.savetxt('VoltCurrMeas.csv', values,fmt = '%s', delimiter=',', newline = '\n')
+mydata = np.genfromtxt('VoltCurrMeas.csv', delimiter=',')
+voltage = mydata[:,0]
+current = mydata[:,1]
+plt.scatter(voltage, current)
+plt.show()
